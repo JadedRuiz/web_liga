fa2d9e4e8376e25188d5379d86dd965aedaf47a6
gilcatzin <gilcatzin@serteza.com>
2022-03-18 12:39:35 -0600

    landingpage

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
- const API_SERTEZA = "http://localhost/api_liga/public/api/";
-//const API_SERTEZA = "https://apiliga.reydelosdeportes.com.mx/public/api/";   
+// const API_SERTEZA = "http://localhost/api_liga/public/api/";
+const API_SERTEZA = "https://apiliga.reydelosdeportes.com.mx/public/api/";   
 
-=======
- //const API_SERTEZA = "http://localhost/api_liga/public/api/";
-const API_SERTEZA = "https://apiliga.reydelosdeportes.com.mx/public/api/";
->>>>>>> e824f110ca6a035aa7f72bc9b2480003f94a1786
 
 export const SERVER_API = API_SERTEZA;
